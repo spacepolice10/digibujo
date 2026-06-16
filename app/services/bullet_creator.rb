130ce36b76f534906d2a9a3f7eb7cb3163a16610
Vlad Kov <spcpolice@Vlads-Air.lan>
2026-06-16 13:57:30 +0700

    feat: add BulletCreator service object

---
+++ app/services/bullet_creator.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class BulletCreator
+  attr_reader :bullet
+
+  def initialize(user, params)
+    @user = user
+    @params = params
+  end
+
+  def call
+    build_bullet
+    if @bullet.save
+      update_bulletable!
+      finalize_content!
+      @bullet.reload
+    end
+    self
+  end
+
+  def success?
+    @bullet.errors.none?
+  end
+
+  private
+
+  def build_bullet
+    type_name = @params[:bulletable_type].presence || 'Task'
+    attributes = @params.except(:bulletable_type, :bulletable_attributes)
+    @bullet = @user.bullets.new(attributes.merge(bulletable: type_name.constantize.new))
+  end
+
+  def update_bulletable!
+    attrs = @params[:bulletable_attributes]
+    return unless attrs.present? && @bullet.bulletable.is_a?(Note)
+
+    @bullet.bulletable.update!(attrs.permit(:mood, :awaits_research, :idea))
+  end
+
+  def finalize_content!
+    body_record = ActionText::RichText.find_by(record: @bullet, name: 'body')
+    @bullet.apply_project_tags_from_content!(rich_text_record: body_record) if body_record
+    @bullet.apply_people_tags_from_content!(rich_text_record: body_record) if body_record
+    @bullet.sanitize_rich_body_tag_attachables!
+    purge_blank_rich_body!
+  end
+
+  def purge_blank_rich_body!
+    return unless @bullet.rich_body.blank?
+
+    ActionText::RichText.find_by(record: @bullet, name: 'rich_body')&.destroy
+  end
+end
---
+++ test/services/bullet_creator_test.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class BulletCreatorTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test 'creates bullet with default type' do
+    result = BulletCreator.new(@user, { body: 'Test body', pops_on: Date.current.iso8601 }).call
+    assert result.success?
+    assert_equal 'Task', result.bullet.bulletable_type
+    assert_equal 'Test body', result.bullet.body.to_plain_text
+  end
+
+  test 'creates bullet with specified type' do
+    result = BulletCreator.new(@user, { bulletable_type: 'Note', body: 'A note' }).call
+    assert result.success?
+    assert_equal 'Note', result.bullet.bulletable_type
+  end
+
+  test 'creates bullet with bucket_id' do
+    collection = create_collection!(@user, name: 'Test')
+    result = BulletCreator.new(@user, { bulletable_type: 'Task', body: 'Collected', bucket_id: collection.bucket.id }).call
+    assert result.success?
+    assert_equal collection.bucket.id, result.bullet.bucket_id
+  end
+
+  test 'tags project from body attachment' do
+    project = create_project!(@user, name: 'Tagged')
+    body_html = ActionText::Content.new('').append_attachables(project).to_html
+    result = BulletCreator.new(@user, { bulletable_type: 'Task', body: body_html }).call
+    assert result.success?
+    assert_includes result.bullet.projects, project
+  end
+
+  test 'fails with validation error' do
+    result = BulletCreator.new(@user, { bulletable_type: 'Task', body: '' }).call
+    assert_not result.success?
+    assert result.bullet.errors[:body].any?
+  end
+
+  test 'purges blank rich_body' do
+    result = BulletCreator.new(@user, { bulletable_type: 'Task', body: 'Only body', rich_body: '' }).call
+    assert result.success?
+    assert_not result.bullet.rich_body?
+  end
+
+  test 'persists rich_body' do
+    result = BulletCreator.new(@user, { bulletable_type: 'Note', body: 'Short', rich_body: '<p>Long detail</p>' }).call
+    assert result.success?
+    assert result.bullet.rich_body?
+    assert_match 'Long detail', result.bullet.rich_body.to_plain_text
+  end
+
+  test 'sets note mood and flags' do
+    result = BulletCreator.new(@user, {
+      bulletable_type: 'Note', body: 'Moody',
+      bulletable_attributes: ActionController::Parameters.new(mood: 'inspired', awaits_research: '1', idea: '1')
+    }).call
+    assert result.success?
+    assert_equal 'inspired', result.bullet.bulletable.mood
+    assert result.bullet.bulletable.awaits_research
+    assert result.bullet.bulletable.idea
+  end
+end
