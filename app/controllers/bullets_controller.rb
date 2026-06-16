b0956afb31b4c677b7284ae6c2db4f2bde721272
Vlad Kov <spcpolice@Vlads-Air.lan>
2026-06-16 13:58:18 +0700

    refactor: use BulletCreator, remove render_context plumbing

--- app/controllers/bullets_controller.rb
+++ app/controllers/bullets_controller.rb
@@ -11,26 +11,16 @@ class BulletsController < ApplicationController
     type_name = params[:bulletable_type].presence || 'Task'
     @bullet.bulletable_type = type_name
     @bullet.bulletable = type_name.constantize.new
-    @composer_frame_id = params[:composer_frame_id]
-    @render_context = params[:render_context]
-    @monthly_bucket_id = params[:monthly_bucket_id]
     @default_project_id = params[:default_project_id]
     @default_person_id = params[:default_person_id]
   end
 
   def create
-    @render_context = params.dig(:bullet, :render_context)
-    @monthly_bucket_id = params.dig(:bullet, :monthly_bucket_id)
-    @composer_frame_id = params[:composer_frame_id]
-    @bullet = create_bullet_from
-    @bullet.pending_attachment_count = Array(params.dig(:bullet, :attachments)).compact_blank.size
-    if @bullet.save
-      attach_pending_files!(@bullet)
-      update_bulletable!(@bullet)
-      finalize_bullet_content!(@bullet)
-      @bullet.reload
+    result = BulletCreator.new(Current.user, bullet_params).call
+    @bullet = result.bullet
+    if result.success?
       respond_to do |format|
-        format.turbo_stream { render_create_turbo_stream }
+        format.turbo_stream
         format.html { redirect_to bullet_path(@bullet) }
       end
     else
@@ -46,8 +36,7 @@ class BulletsController < ApplicationController
   def show; end
 
   def update
-    if @bullet.update(bullet_params.except(:attachments, :bulletable_attributes))
-      attach_pending_files!(@bullet)
+    if @bullet.update(bullet_params.except(:bulletable_attributes))
       update_bulletable!(@bullet)
       finalize_bullet_content!(@bullet)
       BulletActivityRecorder.record_updated!(bullet: @bullet)
@@ -76,28 +65,10 @@ class BulletsController < ApplicationController
 
   def bullet_params
     params.require(:bullet).permit(
-      :body,
-      :rich_body,
-      :pops_on,
-      :bulletable_type,
-      :bucket_id,
+      :body, :rich_body, :pops_on, :bulletable_type, :bucket_id,
       attachments: [],
       bulletable_attributes: %i[mood awaits_research idea]
     )
-  end
-
-  def create_bullet_from
-    permitted = bullet_params
-    type_name = permitted[:bulletable_type].presence || 'Task'
-    attributes = permitted.except(:bulletable_type, :attachments, :bulletable_attributes)
-    Current.user.bullets.new(attributes.merge(bulletable: type_name.constantize.new))
-  end
-
-  def attach_pending_files!(bullet)
-    signed_ids = Array(params.dig(:bullet, :attachments)).compact_blank
-    return if signed_ids.empty?
-
-    bullet.attachments.attach(signed_ids)
   end
 
   def update_bulletable!(bullet)
@@ -119,19 +90,6 @@ class BulletsController < ApplicationController
     return unless bullet.rich_body.blank?
 
     ActionText::RichText.find_by(record: bullet, name: 'rich_body')&.destroy
-  end
-
-  def render_create_turbo_stream
-    template = if @render_context.present? && create_turbo_stream_variant?(@render_context)
-                 "bullets/create.#{@render_context}"
-               else
-                 'bullets/create'
-               end
-    render template
-  end
-
-  def create_turbo_stream_variant?(render_context)
-    lookup_context.exists?("bullets/create.#{render_context}", [], false, [], formats: [:turbo_stream])
   end
 
   def render_invalid_create
