5b06bc2b285967cad4be6015ad93bd30b85680dd
Vlad Kov <spcpolice@Vlads-MacBook-Air.local>
2026-06-22 20:22:01 +0700

    Inline bucket_palette_path and empty ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def bucket_palette_path(bucket)
-    bucketable = bucket.bucketable
-    case bucketable
-    when Collection then collection_path(bucketable)
-    when FutureBucket then future_path
-    when MonthlyBucket then future_monthly_bucket_path(bucketable)
-    else raise ArgumentError, "Unknown bucketable type: #{bucketable.class}"
-    end
-  end
 end
