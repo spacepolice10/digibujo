48e6ea384fda71345e2b673cfd75fe94c89d8714
spacepolice10 <naysayer@hey.com>
2026-08-05 00:41:38 +0700

    feat: add new icons and enhance chat styles for improved layout
    - Introduced new SVG icons for daylog and monthlylog to enhance visual consistency.
    - Updated chat CSS to improve layout responsiveness, including adjustments for overflow and padding.
    - Enhanced tabbar styles to ensure proper sizing and alignment of icons.
    - Refactored calendar controller to improve date navigation functionality.
    - Made minor adjustments to various views for better integration of new styles and icons.

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,13 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["cell"]
-  static values = { date: String }
+  static values = { currentDate: String, date: String }
+
+  connect() {
+    const target = this.cellTargets.find(target => target.dataset.calendarDateParam === this.dateValue)
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth", block: "start" })
+    }
+  }
 
   navigateToDate = (event) => {
-    console.log(event.params)
     const {date} = event.params
     for (const target of this.cellTargets) {
-        if (target.dataset.calendarDateValue === date) {
+        if (target.dataset.calendarDateParam == date) {
             target.setAttribute("aria-selected", "true")
         } else {
             target.setAttribute("aria-selected", "false")
